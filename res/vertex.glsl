70b63a773417bac0543c232f7b6c9e1959440059
Vulji <120482641+Vulji@users.noreply.github.com>
2025-02-20 16:00:57 +0100

    Straight line

--- res/vertex.glsl
+++ res/vertex.glsl
@@ -2,11 +2,12 @@
 
 layout(location = 0) in vec2 in_position;
 uniform float aspect_ratio;
+uniform float time_addition;
 
 void main()
 {
     vec2 position = in_position;
-    position += vec2(0.4f,0.4f);
+    position += vec2(0.4f*time_addition,0.4f);
 
     gl_Position = vec4(position.x/aspect_ratio,position.y, 0., 1.);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -36,6 +36,7 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT); // Exécute concrètement l'action d'appliquer sur tout l'écran la couleur choisie au-dessus
         shader.bind(); // On a besoin qu'un shader soit bind (i.e. "actif") avant de draw(). On en reparle dans la section d'après.
         shader.set_uniform("aspect_ratio",gl::framebuffer_aspect_ratio());
+        shader.set_uniform("time_addition", gl::time_in_seconds());
         rectangle_mesh.draw(); // C'est ce qu'on appelle un "draw call" : on envoie l'instruction à la carte graphique de dessiner notre mesh.
         // Rendu à chaque frame
     }
