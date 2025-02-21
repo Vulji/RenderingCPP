fb7db4268df9450a55b5029aff583fcf58dfdb8f
Vulji <120482641+Vulji@users.noreply.github.com>
2025-02-21 18:11:45 +0100

    UV debug

--- res/fragment.glsl
+++ res/fragment.glsl
@@ -1,10 +1,12 @@
 #version 410
 
 out vec4 out_color;
+
+
 in vec3 vertex_position;
 
 void main()
 {
     //out_color = vec4(1.0, 0.85, 0.0, 1.0);
-    out_color = vec4(vertex_position, 1.);
+    out_color = vec4(uv.x, uv.y, 0., 1.);. 
 }
