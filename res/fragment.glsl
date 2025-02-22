af99324932857d699d1878efdfa0126b0341b7cb
Vulji <120482641+Vulji@users.noreply.github.com>
2025-02-22 14:24:08 +0100

    UV correction

--- res/fragment.glsl
+++ res/fragment.glsl
@@ -4,9 +4,10 @@ out vec4 out_color;
 
 
 in vec3 vertex_position;
+in vec2 uv;
 
 void main()
 {
-    //out_color = vec4(uv.x, uv.y, 0., 1.);. 
-    out_color = vec4(vertex_position, 1.);
+    out_color = vec4(uv.x, uv.y, 0., 1.);
+    //out_color = vec4(vertex_position, 1.);
 }
--- res/vertex.glsl
+++ res/vertex.glsl
@@ -1,17 +1,20 @@
 #version 410
 
 layout(location = 0) in vec3 in_position;
+layout (location = 1) in vec2 aTexCoord;
 uniform float aspect_ratio;
 uniform float time_addition;
 uniform mat4 view_projection_matrix;
 uniform mat4 model_matrix;
 
 out vec3 vertex_position;
+out vec2 uv;
 
 void main()
 {
-    vec3 position = in_position;
+    //vec3 position = in_position;
     vertex_position = in_position;
+    uv = aTexCoord;
     
     mat4 model_view_projection_matrix =  view_projection_matrix /*model_matrix*/;
 
